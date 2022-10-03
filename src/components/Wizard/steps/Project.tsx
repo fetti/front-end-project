9c208fa6404a461162b4ad077db0e519aa11b8d9
fetti <fetterke@gmail.com>
2022-10-03 14:30:15 +0200

    Bugfix
    1st commit - project insert bugfix

--- src/components/Wizard/steps/Project.tsx
+++ src/components/Wizard/steps/Project.tsx
@@ -19,11 +19,11 @@ const Project = ({
     }
 
     if (name === "description" && value.length > 50 && value.length < 500) {
-      setValidate(true);
+      return setValidate(true);
     }
 
     if (name === "description" && value !== "") {
-      setValidate(false);
+      return setValidate(false);
     }
   };
 
@@ -32,36 +32,36 @@ const Project = ({
   }, [projectData]);
 
   return (
-    <div className='flex flex-col'>
-      <div className='w-full mx-2 flex-1'>
-        <div className='font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase'>
+    <div className="flex flex-col">
+      <div className="w-full mx-2 flex-1">
+        <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
           Projekt megnevezése
         </div>
-        <div className='bg-white my-2 p-1 flex border border-gray-200 rounded'>
+        <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
           <input
             onChange={handleChange}
             value={projectData["name"] || ""}
-            name='name'
+            name="name"
             required
-            className='p-1 px-2 appearance-none outline-none w-full text-gray-800'
+            className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
           />
         </div>
       </div>
 
-      <div className='w-full mx-2 flex-1'>
-        <div className='font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase'>
+      <div className="w-full mx-2 flex-1">
+        <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
           Projekt leírása
         </div>
-        <div className='bg-white my-2 p-1 flex border border-gray-200 rounded'>
+        <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
           <textarea
             onChange={handleChange}
             value={projectData["description"] || ""}
-            name='description'
+            name="description"
             minLength={50}
             maxLength={500}
-            className='p-1 px-2 mr-8 appearance-none outline-none w-full text-gray-800'
+            className="p-1 px-2 mr-8 appearance-none outline-none w-full text-gray-800"
           />
-          <p className='text-xs text-gray-300'>
+          <p className="text-xs text-gray-300">
             Nem kötelező! <br /> Minimum 50 maximum 500 karakter!
           </p>
         </div>
